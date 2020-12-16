3f712ce1b820590ee36c383160790f9d831c3b8e
Pablo Peña <pablo@makeitworth.com.mx>
2020-12-16 00:46:54 -0600

    Update: First attempt of refactoring fulfilled
    	We removed all the nested conditionals(and also the unecessary conditionals) from the update_quality method.
    	Now the conditionals from that method checks which "type" of item is being manipulated.
    
    	We move all the logic of modifying quality and sell_in values
    	to specific methods for each type of item.
    
    	We also handle the threshold values in CONSTANTS
    
    	This is not the final solution but the "best" first approach
    	to solve the Kata

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -2,38 +2,85 @@
 
 class GildedRose(object):
 
+    AGED_BRIE = "Aged Brie"
+    BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
+
+    AGED_BRIE_DOUBLE_QUALITY_DECREMENT_SELL_IN_THRESHOLD = 0
+
+    BACKSTAGE_PASSES_DOUBLE_QUALITY_INCREASE_SELL_IN_THRESHOLD = 10
+    BACKSTAGE_PASSES_TRIPLE_QUALITY_INCREASE_SELL_IN_THRESHOLD = 5
+    BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_THRESHOLD = 0
+
+    DEFAULT_ITEM_QUALITY_DECREASE_SELL_IN_THRESHOLD = 0
+
+    MIN_QUALITY = 0
+    MAX_QUALITY = 50
+
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            # existen 4 casos para manipular un item
+            # "Aged Brie", "Backstage passes to a TAFKAL80ETC concert",
+            # "Sulfuras, Hand of Ragnaros" y cuando no es ninguno de
+            # los anteriores
+            # Si el item es AGE_BRIED
+            #  Si el item.quality es menor a la MAX_QUALITY, se incrementa la calidad
+            #
+            if item.name == self.AGED_BRIE:
+                self.decrease_sell_in(item)
+                self.update_aged_brie_quality(item)
+
+            elif item.name == self.BACKSTAGE_PASSES:
+                self.decrease_sell_in(item)
+                self.update_backstage_passes_quality(item)
+
+            elif item.name == self.SULFURAS:
+                pass
+
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                self.decrease_sell_in(item)
+                self.update_default_item_quality(item)
+
+    def increase_quality(self, item):
+        if item.quality < self.MAX_QUALITY:
+            item.quality = item.quality + 1
+
+    def decrease_quality(self, item):
+        if item.quality > self.MIN_QUALITY:
+            item.quality = item.quality - 1
+
+    def decrease_sell_in(self, item):
+        item.sell_in = item.sell_in - 1
+
+    def reset_quality(self, item):
+        item.quality = 0
+
+    def update_aged_brie_quality(self, item):
+        self.increase_quality(item)
+        if item.sell_in < self.AGED_BRIE_DOUBLE_QUALITY_DECREMENT_SELL_IN_THRESHOLD:
+            self.increase_quality(item)
+
+    def update_backstage_passes_quality(self, item):
+        self.increase_quality(item)
+        if item.sell_in <= self.BACKSTAGE_PASSES_DOUBLE_QUALITY_INCREASE_SELL_IN_THRESHOLD:
+            self.increase_quality(item)
+        if item.sell_in <= self.BACKSTAGE_PASSES_TRIPLE_QUALITY_INCREASE_SELL_IN_THRESHOLD:
+            self.increase_quality(item)
+        if item.sell_in <= self.BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_THRESHOLD:
+            self.reset_quality(item)
+
+    def update_default_item_quality(self, item):
+        self.decrease_quality(item)
+        if item.sell_in < self.DEFAULT_ITEM_QUALITY_DECREASE_SELL_IN_THRESHOLD:
+            self.decrease_quality(item)
+
+    def update_sulfuras_quality(self, item):
+        pass
+
+
 
 
 class Item:
@@ -44,3 +91,4 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
